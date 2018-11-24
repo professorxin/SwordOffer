ce5feb0c943ac867d4c8e99220070589bb5781c9
professorxin <1252778220@qq.com>
2018-11-24 16:56:10 +0800

    11.24

--- src/Offer26/Solution.java
+++ src/Offer26/Solution.java
@@ -5,4 +5,47 @@ public class Solution {
     输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。
     要求不能创建任何新的结点，只能调整树中结点指针的指向。*/
 
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+    //定义一个先前指针，指向的是当前有序链表的最末尾元素
+    private TreeNode pre = null;
+    //定义一个头指针，便于返回
+    private TreeNode head = null;
+
+    public TreeNode Convert(TreeNode root) {
+        inOrder(root);
+        return head;
+    }
+
+    //仔细观察可以看到整个的本质是一个中序遍历，在递归调用左儿子和右儿子之间的代码的用于处理指针的指向
+    //因为中序遍历二叉搜索树的结果就是有序的，所以主要是要处理指针的指向
+    private void inOrder(TreeNode node) {
+        //如果结点为空，直接返回
+        if (node == null)
+            return;
+        //左儿子递归调用
+        inOrder(node.left);
+        //当前结点的左儿子指向有序链表的最末尾元素
+        node.left = pre;
+        //如果当前有序链表的最后一个元素非空，他的右儿子就指向该结点
+        if (pre != null)
+            pre.right = node;
+        //让pre指向当前有序链表的最后一个结点
+        pre = node;
+        //这句话只会使用到一次，只是为了确立头指针，用于返回答案
+        if (head == null)
+            head = node;
+        //右儿子递归调用
+        inOrder(node.right);
+    }
 }
--- src/Offer27/Solution.java
+++ src/Offer27/Solution.java
@@ -1,5 +1,9 @@
 package Offer27;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Solution {
     /*题目描述
     输入一个字符串,按字典序打印出该字符串中字符的所有排列。
@@ -7,5 +11,51 @@ public class Solution {
 
     输入描述:
     输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。*/
-    
+
+
+    //牛客网高分答案，采用回溯的思想
+    //帮助理解的图片在images文件夹内
+    public ArrayList<String> Permutation(String str) {
+        //保存所需答案的集合
+        List<String> res = new ArrayList<>();
+        if (str != null && str.length() > 0) {
+            //将str转成数组，传入字符串的首位和保存集合
+            PermutationHelper(str.toCharArray(), 0, res);
+            //对集合的答案排序，形成字典序
+            Collections.sort(res);
+        }
+        return (ArrayList)res;
+    }
+
+    public void PermutationHelper(char[] cs, int i, List<String> list) {
+        //i表示记录当前元素在数组中的位置，如果i位置取出的元素在元素末尾，表明该字符串已经完成排序
+        if (i == cs.length - 1) {
+            //数组转化为字符串
+            String val = String.valueOf(cs);
+            //判断集合中是否包含该字符串，不包含才添加
+            if (!list.contains(val))
+                list.add(val);
+        } else {
+            //令j为i现在的位置，让j后面的每个元素都和i做交换
+            for (int j = i; j < cs.length; j++) {
+                //交换i和j位置对应的元素
+                swap(cs, i, j);
+                //递归调用函数，i+1的原因是交换完一次顺序i就加1，从而才能判断现在i是否在末尾
+                PermutationHelper(cs, i+1, list);
+                //调用完成之后再将i和j调换，恢复之前的顺序来执行下一个for循环
+                swap(cs, i, j);
+            }
+        }
+    }
+
+    public void swap(char[] cs, int i, int j) {
+        char temp = cs[i];
+        cs[i] = cs[j];
+        cs[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Solution p = new Solution();
+        System.out.println(p.Permutation("aa").toString());
+    }
 }
